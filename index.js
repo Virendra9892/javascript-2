22d949ae9f40c1eeb2a1bc14993de44de042423a
Virendra9892 <virendrap9892@gmail.com>
2022-10-14 18:03:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,141 @@
+// console.log("Hey Virendra very good morning !");
+
+// // function sum(a){
+// //     let b = 10;
+// //     let e = a + b;
+// //     console.log("Sum = ",e);
+// //     function sub(){
+// //         let c = a - b;
+// //         console.log("Sub = ",c);
+// //         function mult(){
+// //             let d = a * b;
+// //             console.log("Mult = ",d);
+// //         }
+
+// //     }
+// //     sum(20);
+// //         // sub();
+// //         // mult();
+// // }
+
+// // function sum(a){
+// //     let b = 10;
+// //     let c = a + b;
+// //     console.log("Sum = ",c);
+// //     function sub(d){
+// //         let e = d - a;
+// //         console.log(e);
+
+
+// //     function mult(){
+// //         let f = b * a;
+// //         console.log(f);
+// //     }
+// // }
+// //     mult();
+// //     sub(20);
+
+
+// // }
+// // sum(12);
+
+// function disp(b) {
+//     let a = 10;
+//     function disp1(d) {
+
+//         let c = a + b;
+//         console.log("Sum = ", c);
+//         function disp2(f) {
+//             let e = a - d;
+//             console.log("Sub = ", e);
+
+//             function disp3() {
+//                 let g = f * d;
+//                 console.log("Mult = ", g);
+//             }
+//             disp3();
+//         }
+//         disp2(12);
+//     }
+//     disp1(10);
+// }
+// disp(10);
+
+
+// for(var i=0; i<5;i++){
+//     setTimeout(() => {
+//         console.log(i)
+//     }, 1000);
+// }
+
+// var a = 10;
+// var a  = 20;
+
+// for(var i = 0;i<=10;i++){
+//     function disp(){
+//     console.log(i);
+//     }
+// }
+// disp();
+
+
+// Task Part
+
+
+// Const Part
+
+// console.log(a); Not Allowed
+// const a = 10;
+// console.log(a);
+
+// Var part
+
+// console.log(p);
+// var p = 100;
+
+// var b = 20;
+
+// var b = 10; // Allowed
+
+// function sum(){
+// var c = a + b;
+//     console.log("Sum = ",c);
+// }
+// sum();
+
+//Let part
+
+// console.log(x);  cannot access before initialization.
+
+// let x = 10;
+// let x = 12; // Not allowed 
+// x = 40; // Allowed
+// function sub(){
+//     let z = 5;
+//     let y = x - z;
+//     console.log("Sub = ",y);
+    // console.log(x);
+// }
+
+// sub();
+
+
+// Var - function scope, Allowed Redeclaration, Allowed Reassign, Allowed Hoisting.
+
+// Let - Block Scope, Not Allowed Redeclaration, Not Allowed Reassign, Not Allowed Hoisting.
+
+// Const - Block scope, Not Allowed Redeclaration, Not Allowed Reassign, Not Allowed Hoisting.
+
+// Hoisting - Hoisting means define a variable before its declaration.
+
+// sum();
+// console.log(sum());
+// console.log(sum);
+// function sum(){
+//       console.log("Hello World");
+//       return "Hi Virendra";
+// }
+
+console.log(a);
+// let a = 22;
+var  a = 22;
